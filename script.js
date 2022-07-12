3f2b183bbb562040b07422eafc3ecbd672e7067b
Decodedal <Palumbodallas@gmail.com>
2022-07-11 20:44:37 -0400

    trying to get character movement

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-window.alert('we have java script')
+// window.alert('we have java script')
+let ghost = document.querySelector('#ghost')
+
+function setPositon(item,num1,num2){
+    let one =  item.style.left = num1 + 'px'
+    let two= item.style.bottom = num2 +'px'
+    console.log(one,two)
+    return one, two
+}
+
+setPositon(ghost,100,100)
+
+let x = ghost.style.left
+let y = ghost.style.right
+
+document.addEventListener('keydown',function(e){
+    if(e.key === 'ArrowLeft'){
+        direction = 'left'
+        console.log('left')
+    }
+    if(e.key ==='ArrowUp'){
+        directon = 'up'
+        console.log('up')
+    }
+    if(e.key ==="ArrowDown"){
+        direction = 'down'
+        console.log('down')
+    }
+    if(e.key ==="ArrowRight"){
+        direction = 'right'
+        console.log("right")
+    }
+})
+
+// ghost.addEventListener('click',()=> console.log('hi im ghost'))
