3ab7663f95c138492da10738ce46e7d1d571d68e
Decodedal <Palumbodallas@gmail.com>
2022-07-18 18:47:57 -0400

    up to date

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // window.alert('we have java script')
-let ghostRef = document.querySelector('#ghost')
-let start = document.querySelector('#button')
+let ghostRef = document.querySelector('#ghost');
+let start = document.querySelector('#button');
+let win = false;
 
 function setPositon(item,num1,num2){
     let one =  item.style.left = num1 + 'px'
@@ -30,6 +31,7 @@ document.addEventListener('keydown',function(e){
         }else{
             ghost.x_postion += 6
         }
+        checkKeys()
     }
     if(e.key ==="ArrowRight"){
         ghost.x_postion += 6
@@ -40,6 +42,7 @@ document.addEventListener('keydown',function(e){
         }else{
             ghost.x_postion -= 6
         }
+        checkKeys()
     }
     if(e.key ==='ArrowUp'){
         ghost.y_position -=6
@@ -49,6 +52,7 @@ document.addEventListener('keydown',function(e){
         }else{
             ghost.y_position += 6
         }
+        checkKeys()
     }
     if(e.key ==="ArrowDown"){
         ghost.y_position += 6
@@ -58,6 +62,7 @@ document.addEventListener('keydown',function(e){
         }else{
             ghost.y_position -= 6
         }
+        checkKeys()
     }
   
 })
@@ -154,170 +159,222 @@ function checkMove(){
 
 
 
-let enemys = [
-    {width:50 , height:50, x_postion:1200, y_position:130},
-    {width:50 , height:50, x_postion:800, y_position:430},
-    {width:50 , height:50, x_postion:565, y_position:220},
-]
-
-     let enemysAll = document.querySelectorAll('.enemys') 
-     let myEnemys = []
-     for (let i = 0; i < enemysAll.length; i++){
-        myEnemys.push(enemysAll[i])
-     }
-     
-     myEnemys[0].style.left = enemys[0].x_postion +'px'
-     myEnemys[0].style.top = enemys[0].y_position +'px'
-
-
-
-function  moveenemyOne(){
-   for(let i =0; i < 20; i++){
-   enemys[0].x_postion += i
-   myEnemys[0].style.left= enemys[0].x_postion +'px'
-    console.log(i)
-  }
-}
-button.addEventListener('click',moveenemyOne)
-
-     function checkEnemys(){
-        let result 
-        for (let i = 0; i<walls.length; i++){
-            if (!collisionDetect(enemys[i], walls[i])){
-                 result = false;
-                 break;
-            }else{
-                 result = true;
-            }
-        }
-        // console.log(result)
-        return result
-    }
-
-
-
-
-
-
-
-// //create enemy in the dom
-// function defineEnemy(){
-//     let enemy = document.createElement('img');
-//     enemy.src = "assets/imgs/kisspng-mega-man-11-video-games-pixel-art-sprite-mega-man-megaman-x-8-bit-www-imgkid-com-the-image-kid-h-5cd6b5d4a2f441.4503273315575751246675.png" 
-//     enemy.style.position = 'fixed'
-//     enemy.style.height = '50px'
-//     enemy.style.width =  '50px'
-//     document.body.appendChild(enemy)
-//     return enemy
-// }
-
-// //logic to set enemy movement path 
-// function newEnemy(x_postion,y_position){
-//     let sprite = defineEnemy()
-//     let direction = null;
-
-//     function moveEnemy(){
-//         if (direction === 'left'){
-//             x_postion -= 1
-//             if(checkEnemyHit()){
-//                 console.log('we are hit capitan')
-//             }
-//         }
-//         if( direction === 'right'){
-//             x_postion += 1
-//             if(checkEnemyHit()){
-//                 console.log('we are hit capitan')
-//             }
-//         }
-//         if(direction ==='up'){
-//             y_position -= 1
-//             if(checkEnemyHit()){
-//                 console.log('we are hit capitan')
-//             }
-//         }
-//         if(direction === 'down'){
-//             y_position += 1
-//             if(checkEnemyHit()){
-//                 console.log('we are hit capitan')
-//             }
-//         }
-//         sprite.style.left = x_postion +'px'
-//         sprite.style.top = y_position +'px'
-//     }
-//     setInterval(moveEnemy,10)
-   
-//     function walkUp(time){
-//         direction = 'up'
-//         return new Promise((resolve)=>{
-//             setTimeout(()=>{
-//                 direction = null
-//                 resolve()
-//             },time)
-//         })
-//     }
-//     function walkDown(time){
-//         direction = "down"
-//         return new Promise((resolve)=>{
-//             setTimeout(()=>{
-//                 direction = null
-//                 resolve()
-//             },time)
-//         })
-//     }
-//     function walkLeft(time){
-//         direction = 'left'
-//         return new Promise((resolve)=>{
-//             setTimeout(()=>{
-//                 direction = null
-//                 resolve()
-//             },time)
-//         })
-//     }
-//     function walkRight(time){
-//         direction = 'right'
-//         return new Promise((resolve)=>{
-//             setTimeout(()=>{
-//                 direction = null
-//                 resolve()
-//             },time)
-//         })
-//     }
-//     function stop(){
-//         direction = null
-//     }
-//     return {
-//         sprite :sprite,
-//         walkUp :walkUp,
-//         walkDown: walkDown,
-//         walkLeft :walkLeft,
-//         walkRight :walkRight,
-//         stop:stop
-//     }
-// }
-// //array of new enemys
-// let enemyGroup =[ 
-//     newEnemy(1208, 150),
-//     newEnemy(560 , 233)
+// let enemys = [
+//     {width:50 , height:50, x_postion:1200, y_position:130},
+//     {width:50 , height:50, x_postion:800, y_position:430},
+//     {width:50 , height:50, x_postion:565, y_position:220},
 // ]
-// //defining enemy trajectorys
-// // enemyGroup[0].walkLeft(2500)
-// //       .then(()=> enemyGroup[0].walkDown(1000))
 
-// enemyGroup[1].walkRight(2800)
-//              .then(()=> enemyGroup[1].walkLeft(2800))
+//      let enemysAll = document.querySelectorAll('.enemys') 
+//      let myEnemys = []
+//      for (let i = 0; i < enemysAll.length; i++){
+//         myEnemys.push(enemysAll[i])
+//      }
+     
+//      myEnemys[0].style.left = enemys[0].x_postion +'px'
+//      myEnemys[0].style.top = enemys[0].y_position +'px'
 
 
-//  // function to check if enemy is colliding with ghost     
-//       function checkEnemyHit(){
+
+// function  moveenemyOne(){
+//    while(win == false){
+
+//     enemys[0].x_postion += 1
+//    myEnemys[0].style.left= enemys[0].x_postion +'px'
+//     console.log(enemys[0].x_postion)
+   
+// }
+  
+// }
+// button.addEventListener('click',moveenemyOne)
+
+//      function checkEnemys(){
 //         let result 
-//         for (let i = 0; i<enemyGroup.length; i++){
-//             if (!collisionDetect(ghost, enemyGroup[i])){
+//         for (let i = 0; i<walls.length; i++){
+//             if (!collisionDetect(enemys[i], walls[i])){
 //                  result = false;
 //                  break;
 //             }else{
 //                  result = true;
 //             }
 //         }
-//         console.log(result)
+//         // console.log(result)
 //         return result
 //     }
+
+
+
+
+
+
+
+//create enemy in the dom
+function defineEnemy(){
+    let enemy = document.createElement('img');
+    enemy.src = "assets/imgs/kisspng-mega-man-11-video-games-pixel-art-sprite-mega-man-megaman-x-8-bit-www-imgkid-com-the-image-kid-h-5cd6b5d4a2f441.4503273315575751246675.png" 
+    enemy.style.position = 'fixed'
+    enemy.style.height = '50px'
+    enemy.style.width =  '50px'
+    document.body.appendChild(enemy)
+    return enemy
+}
+
+//logic to set enemy movement path 
+function newEnemy(x_postion,y_position){
+    let sprite = defineEnemy()
+    let direction = null;
+
+    function moveEnemy(){
+        if (direction === 'left'){
+            x_postion -= 1
+            sprite.style.transform = 'scaleX(-1)'
+            if(checkEnemyHit()){
+                console.log('we are hit capitan')
+            }
+        }
+        if( direction === 'right'){
+            x_postion += 1
+            sprite.style.transform = 'scaleX(1)'
+            if(checkEnemyHit()){
+                console.log('we are hit capitan')
+            }
+        }
+        if(direction ==='up'){
+            y_position -= 1
+            if(checkEnemyHit()){
+                console.log('we are hit capitan')
+            }
+        }
+        if(direction === 'down'){
+            y_position += 1
+            if(checkEnemyHit()){
+                console.log('we are hit capitan')
+            }
+        }
+        sprite.style.left = x_postion +'px'
+        sprite.style.top = y_position +'px'
+    }
+    setInterval(moveEnemy,10)
+   
+    function walkUp(time){
+        direction = 'up'
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                direction = null
+                resolve()
+            },time)
+        })
+    }
+    function walkDown(time){
+        direction = "down"
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                direction = null
+                resolve()
+            },time)
+        })
+    }
+    function walkLeft(time){
+        direction = 'left'
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                direction = null
+                resolve()
+            },time)
+        })
+    }
+    function walkRight(time){
+        direction = 'right'
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                direction = null
+                resolve()
+            },time)
+        })
+    }
+    function stop(){
+        direction = null
+    }
+    return {
+        sprite :sprite,
+        walkUp :walkUp,
+        walkDown: walkDown,
+        walkLeft :walkLeft,
+        walkRight :walkRight,
+        stop:stop
+    }
+}
+//array of new enemys
+let enemyGroup =[ 
+    newEnemy(1208, 150),
+    newEnemy(560 , 233)
+]
+//defining enemy trajectorys
+// enemyGroup[0].walkLeft(2500)
+//       .then(()=> enemyGroup[0].walkDown(1000))
+function enemyOneMovment(){
+enemyGroup[1].walkRight(2800)
+             .then(()=>enemyGroup[1].walkUp(1300))
+             .then(()=> enemyGroup[1].walkLeft(2800))
+             .then(()=>enemyGroup[1].walkDown(1300))
+}
+// while(win == false){
+//     enemyOneMovment()
+// }
+
+ // function to check if enemy is colliding with ghost     
+    //   function checkEnemyHit(){
+    //     let result 
+    //     for (let i = 0; i<enemyGroup.length; i++){
+    //         if (!collisionDetect(ghost, enemyGroup[i])){
+    //              result = false;
+    //              break;
+    //         }else{
+    //              result = true;
+    //         }
+    //     }
+    //     console.log(result)
+    //     return result
+    // }
+
+
+
+    //create keys to gather to win game
+    let keys = [
+        {width:50, height:50, x_postion:1028, y_position:228, display:'block'},
+        {width:50, height:50, x_postion:364, y_position:236, display:'block'},
+        {width:50, height:50, x_postion:710, y_position:232, display:'block'}
+    ]
+function drawKeys(){
+    keys.forEach( item =>{
+        let key = document.createElement('img')
+          key.src = "assets/imgs/pngfind.com-zelda-icon-png-6837925.png"
+          key.style.position = 'fixed'
+          key.style.display = `${item.display}`
+          key.style.height = `${item.height}px`
+          key.style.width =  `${item.width}px`
+          key.style.left =  `${item.x_postion}px`
+          key.style.top = `${item.y_position}px`
+          document.body.appendChild(key)
+      })
+    }
+    document.addEventListener('DOMContentLoaded',drawKeys)
+
+    console.log(drawKeys)
+
+      function checkKeys(){
+        let result 
+        for (let i = 0; i<keys.length; i++){
+            if (!collisionDetect(ghost, keys[i])){
+                console.log(keys[i].x_postion)
+                result = false;
+                 keys[i].display = 'none' 
+                 console.log(keys[i].display)
+                 drawKeys()
+            }else{
+                 result = true;
+            }
+        }
+        console.log(result)
+        return result
+    }
